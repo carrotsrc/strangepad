1990f067c0bf863a3c2e3953552ed873e9406d71
Charlie FG <cfg@zunautica.org>
2015-07-16 19:48:09 +0100

    New entry detail

--- src/pad/sys/entry.cpp
+++ src/pad/sys/entry.cpp
@@ -5,9 +5,11 @@
 #include "ui/SHud.hpp"
 #include <iostream>
 
+
 #include "framework/rack/Rack.h"
 #include "framework/memory/BitfieldCache.h"
 
+#include "PadLoader.hpp"
 
 void setupRackoon(RackoonIO::Rack *rack) {
 	std::unique_ptr<RackoonIO::RackUnitGenericFactory> factory(new RackoonIO::RackUnitGenericFactory);
@@ -20,17 +22,9 @@ void setupRackoon(RackoonIO::Rack *rack) {
 	rack->initEvents(0);
 }
 #include <QLibrary>
+typedef QWidget*(*PadBuilder)(const QString &);
 
-QWidget *libraryTest() {
-	QLibrary local("./pads/libui.so");
-	if(!local.load()) {
-		std::cout << "Failed to load ui" << std::endl;
-		return nullptr;
-	}
-
-	local.setLoadHints(QLibrary::ResolveAllSymbolsHint);
-	local.loadHints();
-
+PadBuilder libraryTest() {
 	auto libname = QString("SpSine");
 	auto builder = libname + QString("Build");
 	auto libPath = QString("./pads/libSpSine.so");
@@ -40,17 +34,31 @@ QWidget *libraryTest() {
 		std::cout << "Failed to load lib" << std::endl;
 		return nullptr;
 	}
-	typedef QWidget*(*PadBuilder)(const QString &);
 	auto sym = (PadBuilder) libLoad.resolve(builder.toStdString().c_str());
 	if(!sym) {
 		std::cout << "failed to resolve builder" << std::endl;
+		return nullptr;
 	}
-	return sym("overview");
+
+	return sym;
+}
+
+void initUi() {
+	QLibrary local("./pads/libui.so");
+	if(!local.load()) {
+		std::cout << "Failed to load ui" << std::endl;
+	}
+
+	local.setLoadHints(QLibrary::ResolveAllSymbolsHint);
+	local.loadHints();
 
 }
 
 int main(int argc, char **argv)
 {
+	PadLoader padLoader;
+	padLoader.loadConfig("./.config/pad.xml");
+	return 0;
 	QApplication app (argc, argv);
 	RackoonIO::Rack rack;
 	rack.setConfigPath(".config/pad.cfg");
@@ -62,18 +70,21 @@ int main(int argc, char **argv)
 	qss.open(QFile::ReadOnly);
 	app.setStyleSheet(qss.readAll());
 
-	auto widget = libraryTest();
-	if(!widget) {
+	initUi();
+	auto sym = libraryTest();
+	auto widgetA = sym("overview");
+	auto widgetB = sym("overview");
+	if(!widgetA || !widgetB) {
 		std::cout << "Error loading pad" << std::endl;
 		return -1;
 	}
 
-		std::cout << "Check: " << widget->metaObject()->className() << std::endl;
 	SWindow window;
 	SHud hud("Overview");
 	SHud dhud("Focal");
 
-	hud.addWidget(widget);
+	hud.addWidget(widgetA);
+	hud.addWidget(widgetB);
 	window.addHeadsup(&hud);
 	window.addHeadsup(&dhud);
 	window.show();
