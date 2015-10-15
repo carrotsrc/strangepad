2074c8ce528c6565f865a879af12022b8a33aff8
Charlie Fyvie-Gauld <cfg@zunautica.org>
2015-10-15 13:24:43 +0100

    Add build profiles to strangemake
    Strangemake can build as release or debug

--- src/scripts/strangemake.py
+++ src/scripts/strangemake.py
@@ -16,6 +16,10 @@ fw = os.environ['STRANGEFW']
 sw = os.environ['STRANGEPADFW']
 owd = os.getcwd();
 
+build_profile="release"
+
+
+
 target_order = [
 	'framework',
 
@@ -36,85 +40,80 @@ target_order = [
 targets = {
 	'framework' :{
 			'cd' : fw+"/build/",
-			'cmd' : ['make']
+			'release' : ['make'],
+                        'debug' : ['make']
 			},
 
 	'strangepad' :{
 			'cd' : sw+"/src/pad/",
-			'cmd' : ['make']
+			'release' : ['make'],
+                        'debug' : ['make']
 			},
 
 	'strangeui' :{
 			'cd' : sw+"/src/panels/ui/",
-			'cmd' : ['make']
+			'release' : ['make'],
+                        'debug' : ['make']
 			},
 
 	'unit/SuAlsa' :{
 			'cd' : sw+"/src/units/",
-			'cmd' : ['unitbuild', '../../bin/units/SuAlsa', '-O', '-O3', '-lasound', 'strange/SuAlsa.cpp']
+			'release' : ['unitbuild', '../../bin/units/SuAlsa', '-O', '-O3', '-lasound', 'strange/SuAlsa.cpp'],
+                        'debug' : ['unitbuild', '../../bin/units/SuAlsa', '-g', '-lasound', 'strange/SuAlsa.cpp']
 			},
 
 	'unit/SuFlac' :{
 			'cd' : sw+"/src/units/",
-#			'cmd' : ['unitbuild', '../../bin/units/SuFlac', '-O', '-O3', '-lsndfile', 'strange/SuFlac.cpp']
-			'cmd' : ['unitbuild', '../../bin/units/SuFlac', '-g', '-lsndfile', 'strange/SuFlac.cpp']
+			'release' : ['unitbuild', '../../bin/units/SuFlac', '-O', '-O3', '-lsndfile', 'strange/SuFlac.cpp'],
+			'debug' : ['unitbuild', '../../bin/units/SuFlac', '-g', '-lsndfile', 'strange/SuFlac.cpp']
 			},
 
 	'unit/SuMixer' :{
 			'cd' : sw+"/src/units/",
-			'cmd' : ['unitbuild', '../../bin/units/SuMixer',  '-O', '-O3', 'strange/SuMixer.cpp']
-			#'cmd' : ['unitbuild', '../../bin/units/SuMixer', '-g', 'strange/SuMixer.cpp']
+			'release' : ['unitbuild', '../../bin/units/SuMixer',  '-O', '-O3', 'strange/SuMixer.cpp'],
+			'debug' : ['unitbuild', '../../bin/units/SuMixer', '-g', 'strange/SuMixer.cpp']
 			},
 
 	'unit/SuDelay' :{
 			'cd' : sw+"/src/units/",
-			'cmd' : ['unitbuild', '../../bin/units/SuDelay', '-g', 'strange/SuDelay.cpp']
-			#'cmd' : ['unitbuild', '../../bin/units/SuDelay', '-O', '-O3', 'strange/SuDelay.cpp']
+			'debug' : ['unitbuild', '../../bin/units/SuDelay', '-O', '-O3', 'strange/SuDelay.cpp'],
+                        'release' : ['unitbuild', '../../bin/units/SuDelay', '-g', 'strange/SuDelay.cpp']
 			},
 
 	'unit/SuEncoder' :{
 			'cd' : sw+"/src/units/",
-			'cmd' : ['unitbuild', '../../bin/units/SuEncoder', '-g', 'SuEncoder.cpp']
+			'release' : ['unitbuild', '../../bin/units/SuEncoder', '-O', '-O3', 'SuEncoder.cpp'],
+                        'debug' : ['unitbuild', '../../bin/units/SuEncoder', '-g', 'SuEncoder.cpp']
 			},
 
 	'unit/RuSine' :{
 			'cd' : sw+"/src/units/",
-			'cmd' : ['unitbuild', '../../bin/units/RuSine', '-g', 'RuSine.cpp']
+			'release' : ['unitbuild', '../../bin/units/RuSine', '-O', '-O3', 'RuSine.cpp'],
+                        'debug' : ['unitbuild', '../../bin/units/RuSine', '-g', 'RuSine.cpp']
 			},
-
-#	'unit/BfForderFw' :{
-#			'cd' : sw+"/src/units/basic",
-#			'cmd' : ['unitbuild', '../../../bin/units/basic/BfForderFw', '-g', 'BfForderFw.cpp']
-#			},
-#	'unit/BfSineOsc' :{
-#			'cd' : sw+"/src/units/basic",
-#			'cmd' : ['unitbuild', '../../../bin/units/basic/BfSineOsc', '-g', 'BfSineOsc.cpp']
-#			},
-
-#	'unit/BfForderBw' :{
-#			'cd' : sw+"/src/units/basic",
-#			'cmd' : ['unitbuild', '../../../bin/units/basic/BfForderBw', '-g', 'BfForderBw.cpp']
-#			},
-
 
 	'pad/SpFlac' :{
 			'cd' : sw+"/src/panels/SpFlac/",
-			'cmd' : ['make']
+			'release' : ['make'],
+                        'debug' : ['make'],
 			},
 
 	'pad/SpMixer' :{
 			'cd' : sw+"/src/panels/SpMixer/",
-			'cmd' : ['make']
+			'release' : ['make'],
+                        'debug' : ['make'],
 			},
 
 	'pad/SpSine' :{
 			'cd' : sw+"/src/panels/SpSine/",
-			'cmd' : ['make']
+			'release' : ['make'],
+                        'debug' : ['make'],
 			},
 
 	'pad/SpSine' :{
 			'cd' : sw+"/src/panels/SpDelay/",
-			'cmd' : ['make']
+			'release' : ['make'],
+                        'debug' : ['make'],
 			},
 }
 
@@ -126,12 +125,22 @@ if sys.argv[1] == "-v" or sys.argv[1] == "--version":
 	print("StrangeMaker build script\nv1.0")
 	exit()
 
+if "--release" in sys.argv:
+    build_profile = "release"
+    sys.argv.remove("--release")
+
+if "--debug" in sys.argv:
+    build_profile = "debug"
+    sys.argv.remove("--debug")
+
+print("Build profile: \033[1;35m"+build_profile+"\033[1;m")
+
 if sys.argv[1] == "-a" or sys.argv[1] == "--all":
 	print("Running through all targets...")
 	for target in target_order:
 		print("SMake: \033[1;34m"+target+"\033[1;m")
 		os.chdir(targets[target]['cd'])		
-		args = targets[target]['cmd'] + sys.argv[2:]
+		args = targets[target][build_profile] + sys.argv[2:]
 		if call(args) != 0:
 			print("Target: \033[1;31mFailed\033[1;m")
 			exit()
@@ -149,7 +158,7 @@ os.chdir(targets[sys.argv[1]]['cd'])
 
 print("Building target...")
 
-args = targets[sys.argv[1]]['cmd'] + sys.argv[2:]
+args = targets[sys.argv[1]][build_profile] + sys.argv[2:]
 if "VERBOSE" in os.environ and os.environ['VERBOSE'] == "1":
     print(" ".join(args))
 
